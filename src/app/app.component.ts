234bd4dfaed1ab5a774a5a87f80b0ab73edfbfb3
canojuanpa800 <jcanor@unal.edu.co>
2022-05-02 23:33:45 -0500

    input created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MiAppAngular2';
+  title = 'Mi App Angular 2';
+  subtitle = 'calculadora';
+  number1: number = 0
+  number2: number = 0
+  answer: number = 0
+
+  sumar(): void{
+    this.answer = this.number1 + this.number2
+  }
+  setOperation(): void{
+    alert("mi nombre es +")
+  }
 }
