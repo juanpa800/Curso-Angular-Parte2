ac0d0227584a067b74301931ce42d9a4d8428a46
canojuanpa800 <jcanor@unal.edu.co>
2022-06-14 20:43:21 -0500

    bootstrap grid add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,40 @@ export class AppComponent {
   number2: number = 0
   answer: number = 0
 
-  sumar(): void{
+  add(): void{
     this.answer = this.number1 + this.number2
+  }
+
+  subtract(): void{
+    this.answer = this.number1 - this.number2
+  }
+
+  multiply(): void{
+    this.answer = this.number1 * this.number2
+  }
+
+  divide(): void{
+    if (this.number2 != 0){
+      this.answer = this.number1 / this.number2
+    }else{
+      alert("Can't divide by 0")
+    }
+  }
+
+  operar(X : string): void{
+    let sign = X
+    if (sign == "0"){
+      this.add()
+    }
+    else if (sign == "1"){
+      this.subtract()
+    }
+    else if (sign == "2"){
+      this.multiply()
+    }
+    else if (sign == "3"){
+      this.divide()
+    }
   }
   setOperation(): void{
     alert("mi nombre es +")
